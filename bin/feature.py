87ff7f1f92f78a280cedd0f0f4b6e0aaa640f4cb
RungChi Chen <RungChiChen@sakaes-MacBook-Pro.local>
2016-04-12 12:56:20 +0800

    [Feature] - Upload the modifications

--- bin/feature.py
+++ bin/feature.py
@@ -42,12 +42,12 @@ def feature_engineer(conf, thread, feature_importance, interaction_information, 
         print "Data Distribution is ({}, {}), and then the number of feature is {}".format(np.sum(train_y==0), np.sum(train_y==1), len(names))
 
         # output folder
-        folder_feature = "{}/../feature_profiling".format(BASEPATH)
+        folder_feature = "{}/feature_profiling".format(BASEPATH)
         if not os.path.isdir(folder_feature):
             os.makedirs(folder_feature)
 
         names = list(train_x.columns.values)
-        filepath_feature = "{}/BNP.csv".format(folder_feature)
+        filepath_feature = "{}/feature_profile.csv".format(folder_feature)
 
         fp = feature_engineering.FeatureProfile()
         ranks = fp.profile(train_x.values, train_y, names, filepath_feature, int(len(train_x.columns)*0.5))
@@ -55,8 +55,8 @@ def feature_engineer(conf, thread, feature_importance, interaction_information, 
     if interaction_information:
         log("Try to calculate the interaction information", INFO)
 
-        filepath_training = "{}/../input/train.csv".format(BASEPATH)
-        filepath_testing = "{}/../input/test.csv".format(BASEPATH)
+        filepath_training = "{}/train.csv".format(BASEPATH)
+        filepath_testing = "{}/test.csv".format(BASEPATH)
 
         train_x, test_x, train_y, id_train, id_test = None, None, None, None, None
         if transform2:
@@ -64,10 +64,10 @@ def feature_engineer(conf, thread, feature_importance, interaction_information, 
         else:
             train_x, train_y, test_x, test_id = data_load(drop_fields=drop_fields)
 
-        filepath_cache = "{}/../input/transform2={}_binsize={}_cache.pkl".format(BASEPATH, transform2, binsize)
-        filepath_couple = "{}/../input/transform2={}_testing={}_type=2_binsize={}_combination={}.pkl".format(BASEPATH, transform2, testing, binsize, combinations_size)
-        filepath_series = "{}/../input/transform2={}_testing={}_binsize={}_series.pkl".format(BASEPATH, transform2, testing, binsize)
-        filepath_criteria = "{}/../input/transform2={}_testing={}_binsize={}_criteria.pkl".format(BASEPATH, transform2, testing, binsize)
+        filepath_cache = "{}/transform2={}_binsize={}_cache.pkl".format(BASEPATH, transform2, binsize)
+        filepath_couple = "{}/transform2={}_testing={}_type=2_binsize={}_combination={}.pkl".format(BASEPATH, transform2, testing, binsize, combinations_size)
+        filepath_series = "{}/transform2={}_testing={}_binsize={}_series.pkl".format(BASEPATH, transform2, testing, binsize)
+        filepath_criteria = "{}/transform2={}_testing={}_binsize={}_criteria.pkl".format(BASEPATH, transform2, testing, binsize)
 
         results_couple = feature_engineering.calculate_interaction_information(filepath_cache,\
             train_x, train_y,\
