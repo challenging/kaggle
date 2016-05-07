095a717d816e2efd51cf248d48eff1b1102cdf93
榮錡 陳 <rongqichen@rongqis-iMac.Home>
2016-05-07 09:11:32 +0800

    [Fix] - upload the modifications

--- lib/utils.py
+++ lib/utils.py
@@ -1,8 +1,10 @@
-import logging
 import os
-import copy
+import sys
+
 import md5
-import collections
+import copy
+import logging
+import subprocess
 
 CRITICAL = logging.CRITICAL
 ERROR = logging.ERROR
@@ -51,8 +53,6 @@ def make_a_stamp(model_setting):
     m = md5.new()
     m.update(str(model_setting))
 
-    #m.update(str(collections.OrderedDict(sorted(model_setting.items()))))
-
     return m.hexdigest()
 
 def create_folder(filepath):
@@ -61,3 +61,51 @@ def create_folder(filepath):
     if not os.path.isdir(folder):
         os.makedirs(folder)
         log("Create folder in {}".format(folder), INFO)
+
+def split_file_by_size(filepath, n_size=32*1024*1024):
+    part_no = 1
+    chunk = []
+    with open(filepath, "rb") as INPUT:
+        for line in INPUT:
+            chunk.append(line)
+
+            if sys.getsizeof(chunk) > n_size:
+                filepath_part = "{}.part.{}".format(filepath, part_no)
+                with open(filepath_part, "wb") as OUTPUT:
+                    OUTPUT.writelines(chunk)
+
+                log("write sub-file in {}".format(filepath_part), INFO)
+
+                part_no += 1
+                chunk = []
+
+    filepath_part = "{}.part.{}".format(filepath, part_no)
+    with open(filepath_part, "wb") as OUTPUT:
+        OUTPUT.writelines(chunk)
+
+    log("write sub-file in {}".format(filepath_part), INFO)
+
+def split_file_by_column(filepath, idx, split_char=","):
+    column_name = None
+    folder = os.path.dirname(filepath)
+    filename = os.path.basename(filepath)
+
+    outputs = {}
+
+    with open(filepath, "rb") as INPUT:
+        for line in INPUT:
+            arrs = line.split(split_char)
+            if column_name == None:
+                column_name = arrs[idx]
+            else:
+                value = arrs[idx]
+
+                outputs.setdefault(value, open("{}/{}_site_name={}.csv".format(folder, filename, value), "wb"))
+                outputs[value].write(line)
+
+    for output in outputs.values():
+        output.close()
+
+if __name__ == "__main__":
+    #split_file("/Users/rongqichen/Documents/programs/kaggle/cases/Expedia Hotel Recommendations/input/train/train.csv")
+    split_file_by_column("/Users/rongqichen/Documents/programs/kaggle/cases/Expedia Hotel Recommendations/input/train/train.csv", 1)
