a2223a35873e5e47a0f3d41f10a14f8f6fb0e9a3
榮錡 陳 <rongqichen@rongqis-iMac.Home>
2016-05-23 21:12:39 +0800

    [Feature] - improve the performance by multi-threads

--- lib/facebook/facebook_loader.py
+++ lib/facebook/facebook_loader.py
@@ -11,7 +11,7 @@ from joblib import Parallel, delayed
 
 from utils import log, INFO
 
-def output(df, time_column, time_id, range_x, range_y, size_x, size_y, output_folder):
+def _complex_split_data(df, time_column, time_id, range_x, range_y, size_x, size_y, output_folder):
     c1 = (df[time_column] == time_id)
 
     folder = os.path.join(output_folder, "{}={}".format(time_column, time_id))
@@ -43,32 +43,34 @@ def complex_split_data(filepath, time_column, time_func,
     df = pd.read_csv(filepath)
     df[time_column] = df["time"].map(time_func)
 
-    Parallel(n_jobs=7)(delayed(output)(df, time_column, time_id, range_x, range_y, size_x, size_y, output_folder) for time_id in df[time_column].unique())
+    Parallel(n_jobs=7)(delayed(_complex_split_data)(df, time_column, time_id, range_x, range_y, size_x, size_y, output_folder) for time_id in df[time_column].unique())
 
-def pos_split_data(filepath, range_x=[x for x in range(0, 11, 1)], range_y=[y for y in range(0, 11, 1)], 
+def _pos_split_data(df, x, range_y, window_size_x, window_size_y, output_folder):
+    start_x, end_x = x, min(x+window_size_x, 11)
+    c2 = (df["x"].values >= start_x) & (df["x"].values < end_x)
+
+    for y in range_y:
+         start_y, end_y = y, min(y+window_size_y, 11)
+         c3 = (df["y"].values >= start_y) & (df["y"].values < end_y)
+
+         filepath_output = os.path.join(output_folder, "windown_size={},{}".format(window_size_x, window_size_y), "{}_{}.csv".format(start_x, start_y))
+         if not os.path.exists(filepath_output):
+            folder_output = os.path.dirname(filepath_output)
+            if not os.path.isdir(folder_output):
+                os.makedirs(folder_output)
+
+            final_df = df[c2 & c3]
+            if final_df.shape[0] > 0:
+                final_df.to_csv(filepath_output, index=False)
+                log("Save file in {}".format(filepath_output), INFO)
+
+def pos_split_data(filepath, range_x=[x for x in range(0, 11, 1)], range_y=[y for y in range(0, 11, 1)],
                              size_x=[s for s in range(1, 6)], size_y=[y for y in range(1, 6)],
                              output_folder="."):
+
     df = pd.read_csv(filepath)
-
     for window_size_x, window_size_y in zip(size_x, size_y):
-        for x in range_x:
-            start_x, end_x = x, min(x+window_size_x, 11)
-            c2 = (df["x"].values >= start_x) & (df["x"].values < end_x)
-
-            for y in range_y:
-                start_y, end_y = y, min(y+window_size_y, 11)
-                c3 = (df["y"].values >= start_y) & (df["y"].values < end_y)
-
-                filepath_output = os.path.join(output_folder, "windown_size={},{}".format(window_size_x, window_size_y), "{}_{}.csv".format(start_x, start_y))
-                if not os.path.exists(filepath_output):
-                    folder_output = os.path.dirname(filepath_output)
-                    if not os.path.isdir(folder_output):
-                        os.makedirs(folder_output)
-
-                    final_df = df[c2 & c3]
-                    if final_df.shape[0] > 0:
-                        final_df.to_csv(filepath_output, index=False)
-                        log("Save file in {}".format(filepath_output), INFO)
+        Parallel(n_jobs=8)(delayed(_pos_split_data)(df, x, range_y, window_size_x, window_size_y, output_folder) for x in range_x)
 
 def plot_place_history(filepath):
     history = {}
@@ -89,7 +91,7 @@ def plot_place_history(filepath):
             OUTPUT.write("{},{}\n".format(key, "-".join(values)))
 
 if __name__ == "__main__":
-    folder = "/Users/RungChiChen/Documents/programs/kaggle/cases/Facebook V - Predicting Check Ins/input/original"
+    folder = "/Users/rongqichen/Documents/programs/kaggle/cases/Facebook V - Predicting Check Ins/input/original"
     parent_folder = os.path.dirname(folder)
 
     filepath_train = "{}/train.csv".format(folder)
@@ -101,13 +103,11 @@ if __name__ == "__main__":
     time_column = "hourofday"
     time_func = lambda t: (t/60)%24
 
-    complex_split_data(filepath_train, time_column, time_func, range_x, range_y, size_x, size_y, output_folder=os.path.join(parent_folder, "2_way", "train"))
-    complex_split_data(filepath_test, time_column, time_func, range_x, range_y, size_x, size_y, output_folder=os.path.join(parent_folder, "2_way", "test"))
+    #complex_split_data(filepath_train, time_column, time_func, range_x, range_y, size_x, size_y, output_folder=os.path.join(parent_folder, "2_way", "train"))
+    #complex_split_data(filepath_test, time_column, time_func, range_x, range_y, size_x, size_y, output_folder=os.path.join(parent_folder, "2_way", "test"))
 
-    '''
     pos_split_data(filepath_train, range_x, range_y, size_x, size_y, output_folder=os.path.join(folder.replace("original", ""), "1_way", "train", "pos"))
     pos_split_data(filepath_test, range_x, range_y, size_x, size_y, output_folder=os.path.join(folder.replace("original", ""), "1_way", "test", "pos"))
-    '''
 
     #filepath_time_sort_train = os.path.join(folder, "train_sort=time.csv")
     #plot_place_history(filepath_time_sort_train)/
