3443998719925dddba59ca0aa6217c6442779c82
RungChi Chen <javawokr@gmail.com>
2016-07-09 23:30:27 +0800

    [Feature] - upload the bimgo scripts

---
+++ lib/bimbo/constants.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+
+COMPETITION_NAME = "bimbo_competition"
+
+WORKSPACE = "/Users/rongqichen/Documents/programs/kaggle/cases/Grupo Bimbo Inventory Demand"
+DATA_PATH = os.path.join(WORKSPACE, "input")
+SPLIT_PATH = os.path.join(DATA_PATH, "split")
+
+TRAIN_FILE = os.path.join(DATA_PATH, "train.csv")
+TEST_FILE = os.path.join(DATA_PATH, "test.csv")
+
+TESTING_TRAIN_FILE = os.path.join(DATA_PATH, "train_10000.csv")
+TESTING_TEST_FILE = os.path.join(DATA_PATH, "test_10000.csv")
+
+COLUMN_AGENCY, COLUMN_CHANNEL, COLUMN_ROUTE, COLUMN_PRODUCT, COLUMN_CLIENT = "Agencia_ID", "Canal_ID", "Ruta_SAK", "Producto_ID", "Cliente_ID"
+COLUMNS = {"agency_id": COLUMN_AGENCY,
+           "channel_id": COLUMN_CHANNEL,
+           "route_id": COLUMN_ROUTE,
+           "product_id": COLUMN_PRODUCT,
+           "client_id": COLUMN_CLIENT}
+
+IP_BEANSTALK, PORT_BEANSTALK = "rongqide-Mac-mini.local", 11300
+IP_BEANSTALK = "sakaes-MacBook-Pro.local"
+TIMEOUT_BEANSTALK=1800
+TASK_BEANSTALK = "bimbo_competition"
+
+MONGODB_URL = "mongodb://{}:27017".format(IP_BEANSTALK)
+MONGODB_BATCH_SIZE = 20000
+
+MONGODB_DATABASE, MONGODB_STATS_COLLECTION = "bimbo", "naive_stats"
+MONGODB_COLUMNS = {COLUMN_AGENCY: "agency_id",
+                   COLUMN_CHANNEL: "channel_id",
+                   COLUMN_ROUTE: "route_id",
+                   COLUMN_PRODUCT: "product_id",
+                   COLUMN_CLIENT: "client_id"}
+
+def get_stats_mongo_collection(name):
+    return "{}_{}".format(MONGODB_STATS_COLLECTION, name.lower())
---
+++ lib/bimbo/stats_benastalk.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+import glob
+
+import pandas as pd
+import numpy as np
+
+import json
+import itertools
+
+import pymongo
+import beanstalkc
+
+from utils import log
+from utils import DEBUG, INFO, WARN
+from bimbo.constants import get_stats_mongo_collection
+from bimbo.constants import COMPETITION_NAME, IP_BEANSTALK, PORT_BEANSTALK, TIMEOUT_BEANSTALK, MONGODB_URL, MONGODB_DATABASE, MONGODB_COLUMNS
+from bimbo.constants import COLUMN_AGENCY, COLUMN_CHANNEL, COLUMN_ROUTE, COLUMN_PRODUCT, COLUMN_CLIENT, COLUMNS, SPLIT_PATH
+
+def stats(filepath_train, filepath_test, columns, fixed_column):
+    df_train = pd.read_csv(filepath_train)
+    df_test = pd.read_csv(filepath_test)
+
+    records = []
+    for rid in range(0, df_test.shape[0]):
+        timestamp_start = time.time()
+
+        row_id, week_num, agency_id, channel_id, route_id, client_id, product_id = df_test.values[rid]
+        record = {
+                    "row_id": row_id,
+                    "fixed_column": fixed_column,
+                    "week_num": week_num,
+                    MONGODB_COLUMNS[COLUMN_AGENCY]: agency_id,
+                    MONGODB_COLUMNS[COLUMN_CHANNEL]: channel_id,
+                    MONGODB_COLUMNS[COLUMN_ROUTE]: route_id,
+                    MONGODB_COLUMNS[COLUMN_CLIENT]: client_id,
+                    MONGODB_COLUMNS[COLUMN_PRODUCT]: product_id,
+                 }
+
+        for num in range(len(columns), 0, -1):
+            key = "{}_dimension".format(num)
+            record.setdefault(key, [])
+
+            for combination in itertools.combinations(columns, num):
+                df = df_train.copy()[df_train[COLUMN_PRODUCT] == product_id]
+
+                dimensions = []
+                for criteria in combination:
+                    dimensions.append(criteria.lower())
+                    df = df[df[criteria] == df_test[criteria].values[rid]]
+
+                    if df.shape[0] == 0:
+                        break
+
+                count = df.shape[0]
+                if count > 0:
+                    record[key].append({"|".join(dimensions): count})
+
+            if record[key]:
+                record["matching_count"] = num
+
+                break
+            else:
+                del record[key]
+
+        records.append(record)
+
+        timestamp_end = time.time()
+        log("Cost {:4f} secends to finish {}/{} records for {}".format(timestamp_end-timestamp_start, rid+1, df_test.shape[0], filepath_test), INFO)
+
+    return records
+
+def consumer(task=COMPETITION_NAME):
+    CLIENT = pymongo.MongoClient(MONGODB_URL)
+
+    TALK = beanstalkc.Connection(host=IP_BEANSTALK, port=PORT_BEANSTALK)
+    TALK.watch(task)
+
+    while True:
+        job = TALK.reserve(timeout=TIMEOUT_BEANSTALK)
+        if job:
+            try:
+                o = json.loads(job.body)
+
+                fixed_column = o["fixed_column"]
+                filepath_train, filepath_test = o["filepath_train"], o["filepath_test"]
+
+                collection = CLIENT[MONGODB_DATABASE][get_stats_mongo_collection(fixed_column)]
+
+                columns = MONGODB_COLUMNS.keys()[:]
+                columns.remove(COLUMN_PRODUCT)
+
+                if COLUMNS[fixed_column] in columns:
+                    columns.remove(COLUMNS[fixed_column])
+
+                # create index
+                for column in columns:
+                    collection.create_index(MONGODB_COLUMNS[column])
+
+                records = stats(filepath_train, filepath_test, columns, fixed_column)
+
+                collection.insert_many(records)
+                log("Insert {} records into {}-{}".format(len(records), MONGODB_DATABASE, get_stats_mongo_collection(fixed_column)), INFO)
+
+                job.delete()
+            except Exception as e:
+                log("Error occurs, {}".format(e), WARN)
+
+                raise
+
+    CLIENT.close()
+    TALK.close()
+
+def producer(column, is_testing, task=COMPETITION_NAME):
+    TALK = beanstalkc.Connection(host=IP_BEANSTALK, port=PORT_BEANSTALK)
+    TALK.watch(task)
+
+    files = os.path.join(SPLIT_PATH, COLUMNS[column], "test", "*.csv")
+    for filepath_test in glob.iglob(files):
+        filepath_train = filepath_test.replace("test", "train")
+
+        string = {"fixed_column": column,
+                  "filepath_train": filepath_train,
+                  "filepath_test": filepath_test}
+
+        TALK.put(json.dumps(string), ttr=TIMEOUT_BEANSTALK)
+        log("Put {} into the queue".format(filepath_test), INFO)
+
+        if is_testing:
+            break
+
+    TALK.close()
