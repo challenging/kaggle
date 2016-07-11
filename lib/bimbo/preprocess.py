01b6cd96dab76be3731197d0b1006e459f203842
RungChi Chen <javawokr@gmail.com>
2016-07-11 17:17:43 +0800

    [Feature] - upload the modifications

--- lib/bimbo/preprocess.py
+++ lib/bimbo/preprocess.py
@@ -14,7 +14,7 @@ import Queue
 
 import pandas as pd
 
-from utils import create_folder, log, INFO
+from utils import create_folder, log, INFO, WARN
 from bimbo.constants import COLUMNS, COMPETITION_GROUP_NAME, IP_BEANSTALK, PORT_BEANSTALK, TIMEOUT_BEANSTALK
 
 WORKSPACE = "/Users/rongqichen/Documents/programs/kaggle/cases/Grupo Bimbo Inventory Demand/input"
@@ -35,11 +35,42 @@ class SplitThread(threading.Thread):
         return df[df[column] == value]
 
     def run(self):
+        hostname = socket.gethostname()
+
         while True:
             timestamp_start = time.time()
 
-            output_filepath, column, value = self.queue.get()
-            self.matching(self.df, column, value).to_csv(output_filepath, index=False)
+            output_filepath, filetype, column, value, job = self.queue.get()
+
+            if os.path.exists(output_filepath):
+                log("Found {} so skipping it".format(output_filepath), INFO)
+            else:
+                df = None
+                if hasattr(self, "df"):
+                    df = self.df
+                elif filetype:
+                    if filetype == "train":
+                        df = self.df_train
+                    elif filetype == "test":
+                        df = self.df_test
+                    else:
+                        raise NotImplementError
+                else:
+                    raise NotImplementError
+
+                self.matching(df, column, value).to_csv(output_filepath, index=False)
+
+                if hostname != IP_BEANSTALK:
+                    p = subprocess.Popen(["scp", output_filepath, "RungChiChen@{}:{}".format(IP_BEANSTALK, output_filepath)])
+                    sts = os.waitpid(p.pid, 0)
+                    log("Transfer {} successfully({})".format(output_filepath, sts), INFO)
+
+
+            if job:
+                try:
+                    job.delete()
+                except Exception as e:
+                    log(e, WARN)
 
             self.queue.task_done()
 
@@ -67,17 +98,17 @@ def producer(columns, ip=IP_BEANSTALK, port=PORT_BEANSTALK, task=COMPETITION_GRO
                 if os.path.exists(output_filepath):
                     log("Found {} so skipping it".format(output_filepath), INFO)
                 else:
-                    request = {"filetype": filepath,
+                    request = {"filetype": filetype,
                                "output_filepath": output_filepath,
                                "column": column,
                                "value": unique_value}
-                    talk.put(json.dumps(request))
+                    talk.put(json.dumps(request), TTR=3600)
 
                     log("Put {} into the queue".format(request), INFO)
 
     talk.close()
 
-def consumer(ip=IP_BEANSTALK, port=PORT_BEANSTALK, task=COMPETITION_GROUP_NAME):
+def consumer(ip=IP_BEANSTALK, port=PORT_BEANSTALK, task=COMPETITION_GROUP_NAME, n_jobs=1):
     global WORKSPACE, TRAIN_FILE, TEST_FILE
     df_train = pd.read_csv(TRAIN_FILE)
     df_test = pd.read_csv(TEST_FILE)
@@ -85,30 +116,27 @@ def consumer(ip=IP_BEANSTALK, port=PORT_BEANSTALK, task=COMPETITION_GROUP_NAME):
     talk = beanstalkc.Connection(host=ip, port=port)
     talk.watch(task)
 
-    hostname = socket.gethostname()
+    queue = Queue.Queue()
+    for n in range(0, n_jobs):
+       thread = SplitThread(kwargs={"df_train": df_train, "df_test": df_test, "queue": queue})
+       thread.setDaemon(True)
+       thread.start()
 
     while True:
-        job = talk.reserve(timeout=TIMEOUT_BEANSTALK)
-        if job:
-            timestamp_start = time.time()
+        if queue.qsize() > 128:
+            log("Start to sleep 60 secends", INFO)
+            time.sleep(60)
+        else:
+            job = talk.reserve(timeout=TIMEOUT_BEANSTALK)
+            if job:
+                o = json.loads(job.body)
+                filetype, output_filepath, column, value = o["filetype"], o["output_filepath"], o["column"], o["value"]
 
-            o = json.loads(job.body)
-            filetype, output_filepath, column, value = o["filetype"], o["output_filepath"], o["column"], o["value"]
+                create_folder(output_filepath)
 
-            create_folder(output_filepath)
+                queue.put((output_filepath, filetype, column, value, job))
 
-            SplitThread.matching(df_train if filetype == "train" else df_test, column, value).to_csv(output_filepath, index=False)
-
-            if hostname != ip:
-                p = subprocess.Popen(["scp", output_filepath, "RungChiChen@{}:{}".format(ip, output_filepath)])
-                sts = os.waitpid(p.pid, 0)
-                log("Transfer {} successfully({})".format(output_filepath, sts), INFO)
-
-            job.delete()
-
-            timestamp_end = time.time()
-            log("Cost {:4} secends to save file in {}".format(timestamp_end-timestamp_start, output_filepath), INFO)
-
+    queue.join()
     talk.close()
 
 @click.command()
@@ -122,7 +150,7 @@ def preprocess(mode, columns, n_jobs):
         if mode.lower() == "producer":
             producer(columns)
         else:
-            consumer()
+            consumer(n_jobs=n_jobs)
     else:
         queue = Queue.Queue()
         for filepath in [TRAIN_FILE, TEST_FILE]:
@@ -145,7 +173,7 @@ def preprocess(mode, columns, n_jobs):
                     if os.path.exists(output_filepath):
                         log("Found {} so skipping it".format(output_filepath), INFO)
                     else:
-                        queue.put((output_filepath, column, unique_value))
+                        queue.put((output_filepath, None, column, unique_value, None))
                         log("Put {} into the queue".format(output_filepath), INFO)
 
                 queue.join()
